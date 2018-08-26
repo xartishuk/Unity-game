757a57707f466ff95d5143d2f504e5ff2e1b86c0
xartishuk <xartishuk@gmail.com>
2018-08-27 00:50:33 +0300

    basic movement

---
+++ CityGame/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W) && rb.velocity.y < 50)
+        {
+            rb.AddRelativeForce(10, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.A) && rb.velocity.z > -50)
+        {
+            rb.AddRelativeForce(0, -10, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.S) && rb.velocity.y > -50)
+        {
+            rb.AddRelativeForce(-10, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.D) && rb.velocity.z < 50)
+        {
+            rb.AddRelativeForce(0, 10, 0, ForceMode.Acceleration);
+        }
+    }
+}
